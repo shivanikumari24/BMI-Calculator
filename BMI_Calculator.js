46e557330b2d9b5a0f88b0fc81b3d3cfb871a4e4
shivani jha <78164260+shivanikumari24@users.noreply.github.com>
2023-03-28 21:21:43 +0530

    updated BMI_calculator.js

--- BMI_Calculator.js
+++ BMI_Calculator.js
@@ -10,5 +10,21 @@ console.log(bmi);
 
 
 
-//
+//2nd method by using function
+var interpretation="";
+function bmiCalculator(weight, height){
+    var bmi = weight / (height*height);
+
+    if (bmi < 18.5) {
+       interpretation ="Your BMI is " + bmi + ", so you are underweight.";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        interpretation = "Your BMI is " + bmi +", so you have a normal weight.";
+    } else {
+        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+    }
+
+   return interpretation;
+}
+console.log(interpretation)
+
 
